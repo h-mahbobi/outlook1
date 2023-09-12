772161c755da3c108a66e360840178755d984f81
h-mahbobi <hosein.mahbobi@outlook.com>
2023-09-11 20:47:29 -0700

    new file

---
+++ KafkaUtils.cs
@@ -0,0 +1,71 @@
+﻿using Confluent.Kafka;
+using Microsoft.VisualBasic;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Producer
+{
+    /// <summary>
+    /// Package :
+    ///	  <PackageReference Include="Confluent.Kafka" Version="2.2.0" />
+    /// </summary>
+    public class KafkaUtils
+    {
+        public string Hostname { get; }
+        public string Topic { get; }
+        public string GroupId { get; }
+        private ConsumerConfig consumerConfig { get; }
+        public KafkaUtils(string hostname = "localhost:29092", string groupid = "group1", string topic = "users")
+        {
+            Hostname = hostname;
+            GroupId=groupid;
+            Topic = topic; 
+        }
+
+
+        /// <summary>
+        /// usage :
+        ///  new RabbitUtils().Recive(f => { Console.WriteLine(f); return ""; });        
+        /// </summary>
+        /// <param name="RecivedAct"></param>
+        public void Recive(Func<string, string> RecivedAct)
+        {
+            var consumerConfig = new ConsumerConfig
+            {
+                BootstrapServers = Hostname,
+                GroupId = GroupId,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            using var consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+            consumer.Subscribe(Topic);
+            try
+            {
+                while (true)
+                {
+                    var result = consumer.Consume(TimeSpan.FromSeconds(1));
+                    if (result == null)
+                    {
+                        continue;
+                    }
+                    RecivedAct(result.TopicPartitionOffset + " : "+result.Message.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+
+
+        public void Send(string msg)
+        {
+            var config = new ProducerConfig { BootstrapServers = Hostname};
+
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+            producer.Produce(Topic, new Message<Null, string> { Value = msg });
+            producer.Flush(TimeSpan.FromSeconds(10));
+           
+        }
+    }
+}
---
+++ RabbitUtils.cs
@@ -0,0 +1,92 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Producer
+{
+    public  class RabbitUtils
+    {
+        public string Hostname { get; }
+        public string ExchangeName { get; }
+        public string RouteKey { get; }
+        private ConnectionFactory cnsfactory { get; }
+        public RabbitUtils(string hostname = "localhost", string exchangeName = "ex", string routeKey = "key")
+        {
+            Hostname = hostname;
+            ExchangeName = exchangeName;
+            RouteKey = routeKey;
+
+            cnsfactory = new ConnectionFactory()
+            {
+                HostName = hostname,
+            };
+        }
+
+
+        /// <summary>
+        /// usage :
+        ///  new RabbitUtils().Recive(f => { Console.WriteLine(f); return ""; });        
+    /// </summary>
+    /// <param name="RecivedAct"></param>
+    public  void  Recive(Func<string,string> RecivedAct)
+        {
+            //string Hostname = "localhost";
+            //string ExchangeName = "ex";
+            //string RouteKey = "key";
+
+            //var cnsfactory = new ConnectionFactory()
+            //{
+            //    HostName = Hostname,
+            //};
+
+            using var cns = cnsfactory.CreateConnection();
+            using var chanell = cns.CreateModel();
+
+            chanell.ExchangeDeclare(ExchangeName, ExchangeType.Topic, false, false, null);
+
+            var queueName = chanell.QueueDeclare().QueueName;
+            chanell.QueueBind(queueName, ExchangeName, RouteKey, null);
+
+
+            var consumer = new EventingBasicConsumer(chanell);
+            consumer.Received += Consumer_Received;
+            chanell.BasicConsume(queueName,true, consumer); // can true auto ack
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+            void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+            {
+
+                var body = e.Body.ToArray();
+                var stringmsg = System.Text.Encoding.UTF8.GetString(body);
+                //chanell.BasicAck(e.DeliveryTag, false); // not need to set ack if auto ack true;
+                RecivedAct(stringmsg);
+            }
+
+        }
+
+
+        /// <summary>
+        /// usage :
+        /// RabbitUtils r = new RabbitUtils();
+        ///  r.Send("hello world");
+        /// </summary>
+        /// <param name="msg"></param>
+        public void Send(string msg)
+        {
+            var cnsfactory = new ConnectionFactory()
+            {
+                HostName = this.Hostname,
+            };
+
+            using var cns = cnsfactory.CreateConnection();
+            using var chanell = cns.CreateModel();
+
+            chanell.ExchangeDeclare(this.ExchangeName, ExchangeType.Topic, false, false, null);
+
+            var messageByte = System.Text.Encoding.UTF8.GetBytes(msg).ToArray();
+            chanell.BasicPublish(this.ExchangeName, this.RouteKey, null, messageByte);
+
+        }
+    }
+}
