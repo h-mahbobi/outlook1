a711c23e9dae4affba41dcdd6f57524fa3b686a0
h-mahbobi <hosein.mahbobi@outlook.com>
2023-09-11 20:45:32 -0700

    what

---
+++ GitPush.bat
@@ -0,0 +1,10 @@
+git init
+git remote add origin https://github.com/h-mahbobi/outlook1.git
+git pull origin master
+
+
+git add .
+git status
+set /p input= write commit message : 
+git commit -m  "%input%"
+git push
